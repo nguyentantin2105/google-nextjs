85032adf74be604b32952a95918b7333aa529486
nguyentantin2105 <93tinndu1314@gmail.com>
2021-04-18 00:08:12 +0700

    push a part of UI

---
+++ components/Avatar.js
@@ -0,0 +1,10 @@
+function Avatar({url}) {
+    return (
+        <img loading="lazy" src={url} alt="profile picture"
+            className="h-10 rounded-full cursor-pointer transition duration-150
+                        transform hover:scale-110"
+        />
+    )
+}
+
+export default Avatar
